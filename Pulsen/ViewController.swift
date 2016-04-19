e4f67ab94530d48d2f6a44d5f0f81f84b33ab00e
Kevin Sjöberg <kev.sjoberg@gmail.com>
2016-04-19 18:27:04 +0200

    Initial commit

---
+++ Pulsen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pulsen
+//
+//  Created by Kevin Sjöberg on 19/04/16.
+//  Copyright © 2016 White Robba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
